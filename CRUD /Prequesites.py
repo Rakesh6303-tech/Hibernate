ad57e66f6eeeefbd127c91448c82bce07532d82c
Rakesh <dr8452799@gmail.com>
2025-07-24 14:21:49 +0530

    Create Prequesites.py

---
+++ CRUD /Prequesites.py
@@ -0,0 +1,20 @@
+When we are Running some applications by using hibernate frame work we must create 4 files
+1) pom.xml---> conatins Dependency like hibernate, mysql connector
+2) hibernate.cfg.xml(Name should be anything but extenson should be xml) 
+            -----> JdbcDriver,Connection url, username, password, mysql dilect etc.
+3) Entity Class ---> Contains Annotations and Constructors etc.
+4) App.class or Launch.class----> Contains some steps 
+        // Create Configuration & load 
+        1) Configuration config = new Configuration(); 
+        // Add Annotated Class to Configuration
+        2) config.addAnnotatedClass(classname.class);
+        // Build sessionFactory and Open Session
+        3) SessionFactory sessionfactory = config.buildSessionFactory();
+           Session session = sessionfactory.opendSession();
+        // Begin Transaction 
+        4) Transcation transaction = session.beginTransaction();
+        // Save Object to Database
+        5) Employee e = new Employee(1,'rajesh');
+           session.save(e);
+        //  Commit Transaction to save data into the database
+         6) session.commit();
